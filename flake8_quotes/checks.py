fa036c82b1c86de2307410e77176288c1438869f
Evan Klitzke <evan@eklitzke.org>
2014-10-22 15:57:16 -0700

    fixes for 0.0.2

--- flake8_quotes/checks.py
+++ flake8_quotes/checks.py
@@ -1,10 +1,10 @@
 import re
 from flake8_quotes import __version__
 
+
 def check_quotes(logical_line):
-    """Raise lint error when using " instead of '"""
-    # if there is no double quote sign
-    # there's nothing to do
+    """Raise lint error when using " instead of '."""
+    # if there is no double quote sign there's nothing to do
     if logical_line.find('"') == -1:
         return
 
@@ -12,11 +12,9 @@ def check_quotes(logical_line):
     if logical_line.strip().startswith('#'):
         return
 
-    # # if it's a multilogical_line string, is
-    # # ok to have doublequotes
+    # if it's a multilogical_line string, is ok to have doublequotes
     if logical_line.find('"""') != -1:
         return
-
 
     # ignore double quotes wrapped in singlequotes
     singles = re.match(r"'(.*)'", logical_line)
