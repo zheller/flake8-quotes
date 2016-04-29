zheller/flake8-quotes

commit 2ea1ab45909797604426f43f9f86ef1395f50111

Add support for defining multiline quote type
 - Improve naming and documentation of QUOTES and MULTILINE_QUOTES
   dictionaries inside `QuoteChecker`.
 - Add new `--multiline-quotes` option to allow customisation of
   multiline quote type.
 - Improve logic inside `QuoteChecker.get_quotes_errors` so that we can
   check single and multiline quotes separately.
 - Add new tests for mixed strings and fix tests broken by the new
   multiline rules.

flake8_quotes/__init__.py
@@ -10,19 +10,35 @@ class QuoteChecker(object):
     version = __version__
 
     QUOTES = {
-        # When user wants only single quotes
+        # Allow single quotes
+        # Fail on double quotes
         '\'': {
-            'good_single': '\'',
-            'good_multiline': '\'\'\'',
-            'bad_single': '"',
-            'bad_multiline': '"""',
+            'allow': '\'',
+            'fail': '"',
         },
+
+        # Allow double quotes
+        # Fail on single quotes
         '"': {
-            'good_single': '"',
-            'good_multiline': '"""',
-            'bad_single': '\'',
-            'bad_multiline': '\'\'\'',
+            'allow': '"',
+            'fail': '\'',
         },
+    }
+
+    MULTILINE_QUOTES = {
+        # Allow single multiline quotes
+        # Fail on double multiline quotes
+        '\'': {
+            'allow': '\'\'\'',
+            'fail': '"""'
+        },
+
+        # Allow double multiline quotes
+        # Fail on single multiline quotes
+        '"': {
+            'allow': '"""',
+            'fail': '\'\'\''
+        }
     }
 
     def __init__(self, tree, filename='(none)', builtins=None):
@@ -32,11 +48,19 @@ class QuoteChecker(object):
     def add_options(cls, parser):
         parser.add_option('--quotes', default='\'', action='store',
                           help='Quote to expect in all files (default: \')')
-        parser.config_options.append('quotes')
+        parser.add_option('--multiline-quotes', default='\'', action='store',
+                          help='Multiline quote to expect in all files (default: \')')
+        parser.config_options.extend(['quotes', 'multiline_quotes'])
 
     @classmethod
     def parse_options(cls, options):
+        if hasattr(options, 'multiline_quotes'):
+            multiline_quotes = options.multiline_quotes
+        else:
+            multiline_quotes = options.quotes
+
         cls.quotes = cls.QUOTES[options.quotes]
+        cls.multiline_quotes = cls.MULTILINE_QUOTES[multiline_quotes]
 
     def get_file_contents(self):
         if self.filename in ('stdin', '-', None):
@@ -68,16 +92,16 @@ class QuoteChecker(object):
                 # ignore non strings
                 continue
 
-            if not token.string.startswith(self.quotes['bad_single']):
-                # ignore strings that do not start with our quotes
+            if not token.string.startswith(self.quotes['fail']) and not token.string.startswith(self.multiline_quotes['fail']):
+                # ignore strings that do not start with our quotes (both single and multiline)
                 continue
 
-            if token.string.startswith(self.quotes['bad_multiline']):
-                # ignore multiline strings
+            if self.quotes['allow'] in token.string:
+                # ignore quotes wrapped in our quotes (e.g. `'` in `"it's"`)
                 continue
 
-            if self.quotes['good_single'] in token.string:
-                # ignore alternate quotes wrapped in our quotes (e.g. `'` in `"it's"`)
+            if self.multiline_quotes['allow'] in token.string:
+                # ignore multiline quotes wrapped in our quotes
                 continue
 
             start_row, start_col = token.start

test/data/doubles_multiline_string.py
@@ -1,7 +1,3 @@
-""" This "should"
-"not" be
-"linted" """
-
 ''' This "should"
 "not" be
-linted "either" '''
+"linted" '''

test/data/singles_multiline_string.py
@@ -1,7 +1,3 @@
-''' This 'should'
-'not' be
-'linted' '''
-
 """ This 'should'
 'not' be
-linted 'either' """
+'linted' """

test/data/doubles_mixed.py
@@ -1,0 +1,5 @@
+this_should_not_be_linted = 'single quote string'
+
+"""double quote docstring (should not be linted either)"""
+
+"""mixed 'double' quote 'docstring' (should not be linted either)"""

test/data/singles_mixed.py
@@ -1,0 +1,5 @@
+this_should_not_be_linted = "double quote string"
+
+'''single quote docstring (should not be linted either)'''
+
+'''mixed "single" quote "docstring" (should not be linted either)'''

test/test_checks.py
@@ -34,6 +34,18 @@ class DoublesTestChecks(TestCase):
         self.assertEqual(list(checker.run()), [])
 
 
+class DoublesMixedTestChecks(TestCase):
+    def setUp(self):
+        class DoublesMixedOptions():
+            quotes = '\''
+            multiline_quotes = '"'
+        QuoteChecker.parse_options(DoublesMixedOptions)
+
+    def test_mixed(self):
+        doubles_checker = QuoteChecker(None, filename=get_absolute_path('data/doubles_mixed.py'))
+        self.assertEqual(list(doubles_checker.get_quotes_errors(doubles_checker.get_file_contents())), [])
+
+
 class SinglesTestChecks(TestCase):
     def setUp(self):
         class SinglesOptions():
@@ -57,6 +69,18 @@ class SinglesTestChecks(TestCase):
     def test_noqa_singles(self):
         checker = QuoteChecker(None, get_absolute_path('data/singles_noqa.py'))
         self.assertEqual(list(checker.run()), [])
+
+
+class SinglesMixedTestChecks(TestCase):
+    def setUp(self):
+        class SinglesMixedOptions():
+            quotes = '"'
+            multiline_quotes = '\''
+        QuoteChecker.parse_options(SinglesMixedOptions)
+
+    def test_mixed(self):
+        doubles_checker = QuoteChecker(None, filename=get_absolute_path('data/singles_mixed.py'))
+        self.assertEqual(list(doubles_checker.get_quotes_errors(doubles_checker.get_file_contents())), [])
 
 
 def get_absolute_path(filepath):

commit 3fda3d44e7fcf43a5173383a24310ac2c8d56915

Improve backwards compatibility for multiline quotes
 - Set default value for `--multiline-quotes` to an empty string,
   signifying that any multiline quotes can be used.
 - Add support for ignoring multiline quotes.
 - Revert test cases broken by 2ea1ab4.

flake8_quotes/__init__.py
@@ -44,19 +44,27 @@ class QuoteChecker(object):
     def add_options(cls, parser):
         parser.add_option('--quotes', default='\'', action='store',
                           help='Quote to expect in all files (default: \')')
-        parser.add_option('--multiline-quotes', default='\'', action='store',
-                          help='Multiline quote to expect in all files (default: \')')
+        parser.add_option('--multiline-quotes', default='', action='store',
+                          help='Multiline quote to expect in all files (default: any)')
         parser.config_options.extend(['quotes', 'multiline_quotes'])
 
     @classmethod
     def parse_options(cls, options):
+        multiline_ignored = True
+
         if hasattr(options, 'multiline_quotes'):
             multiline_quotes = options.multiline_quotes
+
+            if len(multiline_quotes) == 0:
+                multiline_quotes = options.quotes
+            else:
+                multiline_ignored = False
         else:
             multiline_quotes = options.quotes
 
         cls.quotes = cls.INLINE_QUOTES[options.quotes]
         cls.multiline_quotes = cls.MULTILINE_QUOTES[multiline_quotes]
+        cls.multiline_ignored = multiline_ignored
 
     def get_file_contents(self):
         if self.filename in ('stdin', '-', None):
@@ -90,6 +98,10 @@ class QuoteChecker(object):
 
             if not token.string.startswith(self.quotes['bad']) and not token.string.startswith(self.multiline_quotes['bad']):
                 # ignore strings that do not start with our quotes (both single and multiline)
+                continue
+
+            if self.multiline_ignored and any([token.string.startswith(value) for value in self.multiline_quotes.values()]):
+                # ignore any type of multiline if multilines are being ignored
                 continue
 
             if self.quotes['good'] in token.string:

test/data/doubles_multiline_string.py
@@ -1,3 +1,7 @@
+""" This "should"
+"not" be
+"linted" """
+
 ''' This "should"
 "not" be
-"linted" '''
+linted "either" '''

test/data/singles_multiline_string.py
@@ -1,3 +1,7 @@
+''' This 'should'
+'not' be
+'linted' '''
+
 """ This 'should'
 'not' be
-'linted' """
+linted 'either' """