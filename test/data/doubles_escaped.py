0b289c72766b28d9bb5c1f317b1f2cdc50ae7f05
Joe Hitchen <hitchenjoe@gmail.com>
2019-04-23 15:17:51 +0100

    Removed checking of escaped quotes for raw string literals

--- test/data/doubles_escaped.py
+++ test/data/doubles_escaped.py
@@ -2,3 +2,5 @@ this_should_raise_Q003 = 'This is a \'string\''
 this_is_fine = '"This" is a \'string\''
 this_is_fine = "This is a 'string'"
 this_is_fine = "\"This\" is a 'string'"
+this_is_fine = r'This is a \'string\''
+this_is_fine = br'This is a \'string\''
