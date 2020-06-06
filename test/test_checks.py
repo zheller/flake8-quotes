e5b8e8c904cb946aee939a7b85397116244028a1
Todd Wolfson <todd@twolfson.com>
2020-06-06 00:55:39 -0700

    Attempted FileChecker usage, same issues as FileProcessor

--- test/test_checks.py
+++ test/test_checks.py
@@ -26,12 +26,7 @@ class TestFlake8Stdin(TestCase):
         self.assertRegexpMatches(stdout_lines[1], b'stdin:2:(24|25): Q000 Remove bad quotes')
         self.assertRegexpMatches(stdout_lines[2], b'stdin:3:(24|25): Q000 Remove bad quotes')
 
-from flake8.checker import FileProcessor
-
-class EmptyOptions:
-    hang_closing = None
-    max_line_length = None
-    max_line_length = None
+from flake8.checker import FileChecker
 
 class DoublesTestChecks(TestCase):
     def setUp(self):
@@ -41,7 +36,8 @@ class DoublesTestChecks(TestCase):
         QuoteChecker.parse_options(DoublesOptions)
 
     def test_multiline_string(self):
-        foo = FileProcessor(get_absolute_path('data/doubles_multiline_string.py'), EmptyOptions)
+        foo = FileChecker(get_absolute_path('data/doubles_multiline_string.py'), QuoteChecker, QuoteChecker)
+        print(config_options)
         doubles_checker = QuoteChecker(None, filename=get_absolute_path('data/doubles_multiline_string.py'))
         self.assertEqual(list(doubles_checker.get_quotes_errors(doubles_checker.get_file_contents())), [
             {'col': 4, 'line': 1, 'message': 'Q001 Remove bad quotes from multiline string'},
