7ffbf359eebc557c803df4f06a1060eb28c9fa56
Todd Wolfson <todd@twolfson.com>
2021-08-23 00:47:38 -0700

    Updated test assertions to be self-contained

--- test/test_docstring_checks.py
+++ test/test_docstring_checks.py
@@ -16,13 +16,12 @@ class DocstringTestChecks(TestCase):
             None,
             filename=get_absolute_path('data/docstring_doubles.py')
         )
-        message = 'Q001 Double quoted multiline string literal found but single quotes are preferred'
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 0, 'line': 5, 'message': message},
-            {'col': 4, 'line': 16, 'message': message},
-            {'col': 20, 'line': 21, 'message': message},
-            {'col': 8, 'line': 30, 'message': message},
-            {'col': 12, 'line': 35, 'message': message},
+            {'col': 0, 'line': 5, 'message': 'Q001 Double quote multiline found but single quotes preferred'},
+            {'col': 4, 'line': 16, 'message': 'Q001 Double quote multiline found but single quotes preferred'},
+            {'col': 20, 'line': 21, 'message': 'Q001 Double quote multiline found but single quotes preferred'},
+            {'col': 8, 'line': 30, 'message': 'Q001 Double quote multiline found but single quotes preferred'},
+            {'col': 12, 'line': 35, 'message': 'Q001 Double quote multiline found but single quotes preferred'},
         ])
 
         multiline_checker = QuoteChecker(
@@ -30,8 +29,8 @@ class DocstringTestChecks(TestCase):
             filename=get_absolute_path('data/docstring_doubles_module_multiline.py')
         )
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 0, 'line': 4, 'message': message},
-            {'col': 0, 'line': 9, 'message': message},
+            {'col': 0, 'line': 4, 'message': 'Q001 Double quote multiline found but single quotes preferred'},
+            {'col': 0, 'line': 9, 'message': 'Q001 Double quote multiline found but single quotes preferred'},
         ])
 
         multiline_checker = QuoteChecker(
@@ -39,8 +38,8 @@ class DocstringTestChecks(TestCase):
             filename=get_absolute_path('data/docstring_doubles_module_singleline.py')
         )
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 0, 'line': 2, 'message': message},
-            {'col': 0, 'line': 6, 'message': message},
+            {'col': 0, 'line': 2, 'message': 'Q001 Double quote multiline found but single quotes preferred'},
+            {'col': 0, 'line': 6, 'message': 'Q001 Double quote multiline found but single quotes preferred'},
         ])
 
         multiline_checker = QuoteChecker(
@@ -48,8 +47,8 @@ class DocstringTestChecks(TestCase):
             filename=get_absolute_path('data/docstring_doubles_class.py')
         )
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 4, 'line': 3, 'message': message},
-            {'col': 22, 'line': 5, 'message': message},
+            {'col': 4, 'line': 3, 'message': 'Q001 Double quote multiline found but single quotes preferred'},
+            {'col': 22, 'line': 5, 'message': 'Q001 Double quote multiline found but single quotes preferred'},
         ])
 
         multiline_checker = QuoteChecker(
@@ -57,11 +56,11 @@ class DocstringTestChecks(TestCase):
             filename=get_absolute_path('data/docstring_doubles_function.py')
         )
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 4, 'line': 3, 'message': message},
-            {'col': 4, 'line': 11, 'message': message},
-            {'col': 38, 'line': 15, 'message': message},
-            {'col': 4, 'line': 17, 'message': message},
-            {'col': 4, 'line': 21, 'message': message},
+            {'col': 4, 'line': 3, 'message': 'Q001 Double quote multiline found but single quotes preferred'},
+            {'col': 4, 'line': 11, 'message': 'Q001 Double quote multiline found but single quotes preferred'},
+            {'col': 38, 'line': 15, 'message': 'Q001 Double quote multiline found but single quotes preferred'},
+            {'col': 4, 'line': 17, 'message': 'Q001 Double quote multiline found but single quotes preferred'},
+            {'col': 4, 'line': 21, 'message': 'Q001 Double quote multiline found but single quotes preferred'},
         ])
 
     def test_require_single_docstring_double_present(self):
@@ -75,11 +74,10 @@ class DocstringTestChecks(TestCase):
             None,
             filename=get_absolute_path('data/docstring_doubles.py')
         )
-        message = 'Q002 Double quoted docstring literal found but single quotes are preferred'
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 0, 'line': 1, 'message': message},
-            {'col': 4, 'line': 12, 'message': message},
-            {'col': 8, 'line': 24, 'message': message},
+            {'col': 0, 'line': 1, 'message': 'Q002 Double quote docstring found but single quotes preferred'},
+            {'col': 4, 'line': 12, 'message': 'Q002 Double quote docstring found but single quotes preferred'},
+            {'col': 8, 'line': 24, 'message': 'Q002 Double quote docstring found but single quotes preferred'},
         ])
 
         multiline_checker = QuoteChecker(
@@ -87,7 +85,7 @@ class DocstringTestChecks(TestCase):
             filename=get_absolute_path('data/docstring_doubles_module_multiline.py')
         )
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 0, 'line': 1, 'message': message},
+            {'col': 0, 'line': 1, 'message': 'Q002 Double quote docstring found but single quotes preferred'},
         ])
 
         multiline_checker = QuoteChecker(
@@ -95,7 +93,7 @@ class DocstringTestChecks(TestCase):
             filename=get_absolute_path('data/docstring_doubles_module_singleline.py')
         )
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 0, 'line': 1, 'message': message},
+            {'col': 0, 'line': 1, 'message': 'Q002 Double quote docstring found but single quotes preferred'},
         ])
 
         multiline_checker = QuoteChecker(
@@ -103,9 +101,9 @@ class DocstringTestChecks(TestCase):
             filename=get_absolute_path('data/docstring_doubles_class.py')
         )
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 4, 'line': 2, 'message': message},
-            {'col': 8, 'line': 6, 'message': message},
-            {'col': 28, 'line': 9, 'message': message},
+            {'col': 4, 'line': 2, 'message': 'Q002 Double quote docstring found but single quotes preferred'},
+            {'col': 8, 'line': 6, 'message': 'Q002 Double quote docstring found but single quotes preferred'},
+            {'col': 28, 'line': 9, 'message': 'Q002 Double quote docstring found but single quotes preferred'},
         ])
 
         multiline_checker = QuoteChecker(
@@ -113,8 +111,8 @@ class DocstringTestChecks(TestCase):
             filename=get_absolute_path('data/docstring_doubles_function.py')
         )
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 4, 'line': 2, 'message': message},
-            {'col': 4, 'line': 8, 'message': message},
+            {'col': 4, 'line': 2, 'message': 'Q002 Double quote docstring found but single quotes preferred'},
+            {'col': 4, 'line': 8, 'message': 'Q002 Double quote docstring found but single quotes preferred'},
         ])
 
     def test_require_double_docstring_single_present(self):
@@ -128,11 +126,10 @@ class DocstringTestChecks(TestCase):
             None,
             filename=get_absolute_path('data/docstring_singles.py')
         )
-        message = 'Q002 Double quoted docstring literal found but single quotes are preferred'
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 0, 'line': 1, 'message': message},
-            {'col': 4, 'line': 14, 'message': message},
-            {'col': 8, 'line': 26, 'message': message},
+            {'col': 0, 'line': 1, 'message': 'Q002 Single quote docstring found but double quotes preferred'},
+            {'col': 4, 'line': 14, 'message': 'Q002 Single quote docstring found but double quotes preferred'},
+            {'col': 8, 'line': 26, 'message': 'Q002 Single quote docstring found but double quotes preferred'},
         ])
 
         multiline_checker = QuoteChecker(
@@ -140,7 +137,7 @@ class DocstringTestChecks(TestCase):
             filename=get_absolute_path('data/docstring_singles_module_multiline.py')
         )
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 0, 'line': 1, 'message': message},
+            {'col': 0, 'line': 1, 'message': 'Q002 Single quote docstring found but double quotes preferred'},
         ])
 
         multiline_checker = QuoteChecker(
@@ -148,7 +145,7 @@ class DocstringTestChecks(TestCase):
             filename=get_absolute_path('data/docstring_singles_module_singleline.py')
         )
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 0, 'line': 1, 'message': message},
+            {'col': 0, 'line': 1, 'message': 'Q002 Single quote docstring found but double quotes preferred'},
         ])
 
         multiline_checker = QuoteChecker(
@@ -156,9 +153,9 @@ class DocstringTestChecks(TestCase):
             filename=get_absolute_path('data/docstring_singles_class.py')
         )
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 4, 'line': 2, 'message': message},
-            {'col': 8, 'line': 6, 'message': message},
-            {'col': 28, 'line': 9, 'message': message},
+            {'col': 4, 'line': 2, 'message': 'Q002 Single quote docstring found but double quotes preferred'},
+            {'col': 8, 'line': 6, 'message': 'Q002 Single quote docstring found but double quotes preferred'},
+            {'col': 28, 'line': 9, 'message': 'Q002 Single quote docstring found but double quotes preferred'},
         ])
 
         multiline_checker = QuoteChecker(
@@ -166,8 +163,8 @@ class DocstringTestChecks(TestCase):
             filename=get_absolute_path('data/docstring_singles_function.py')
         )
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 4, 'line': 2, 'message': message},
-            {'col': 4, 'line': 8, 'message': message},
+            {'col': 4, 'line': 2, 'message': 'Q002 Single quote docstring found but double quotes preferred'},
+            {'col': 4, 'line': 8, 'message': 'Q002 Single quote docstring found but double quotes preferred'},
         ])
 
     def test_require_single_docstring_single_present(self):
@@ -178,14 +175,13 @@ class DocstringTestChecks(TestCase):
         QuoteChecker.parse_options(Options)
 
         multiline_checker = QuoteChecker(None, filename=get_absolute_path('data/docstring_singles.py'))
-        message = 'Q001 Double quoted multiline string literal found but single quotes are preferred'
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 0, 'line': 5, 'message': message},
-            {'col': 20, 'line': 11, 'message': message},
-            {'col': 4, 'line': 18, 'message': message},
-            {'col': 20, 'line': 23, 'message': message},
-            {'col': 8, 'line': 32, 'message': message},
-            {'col': 12, 'line': 37, 'message': message},
+            {'col': 0, 'line': 5, 'message': 'Q001 Single quote multiline found but double quotes preferred'},
+            {'col': 20, 'line': 11, 'message': 'Q001 Single quote multiline found but double quotes preferred'},
+            {'col': 4, 'line': 18, 'message': 'Q001 Single quote multiline found but double quotes preferred'},
+            {'col': 20, 'line': 23, 'message': 'Q001 Single quote multiline found but double quotes preferred'},
+            {'col': 8, 'line': 32, 'message': 'Q001 Single quote multiline found but double quotes preferred'},
+            {'col': 12, 'line': 37, 'message': 'Q001 Single quote multiline found but double quotes preferred'},
         ])
 
         multiline_checker = QuoteChecker(
@@ -193,8 +189,8 @@ class DocstringTestChecks(TestCase):
             filename=get_absolute_path('data/docstring_singles_module_multiline.py')
         )
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 0, 'line': 4, 'message': message},
-            {'col': 0, 'line': 9, 'message': message},
+            {'col': 0, 'line': 4, 'message': 'Q001 Single quote multiline found but double quotes preferred'},
+            {'col': 0, 'line': 9, 'message': 'Q001 Single quote multiline found but double quotes preferred'},
         ])
 
         multiline_checker = QuoteChecker(
@@ -202,8 +198,8 @@ class DocstringTestChecks(TestCase):
             filename=get_absolute_path('data/docstring_singles_module_singleline.py')
         )
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 0, 'line': 2, 'message': message},
-            {'col': 0, 'line': 6, 'message': message},
+            {'col': 0, 'line': 2, 'message': 'Q001 Single quote multiline found but double quotes preferred'},
+            {'col': 0, 'line': 6, 'message': 'Q001 Single quote multiline found but double quotes preferred'},
         ])
 
         multiline_checker = QuoteChecker(
@@ -211,8 +207,8 @@ class DocstringTestChecks(TestCase):
             filename=get_absolute_path('data/docstring_singles_class.py')
         )
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 4, 'line': 3, 'message': message},
-            {'col': 22, 'line': 5, 'message': message},
+            {'col': 4, 'line': 3, 'message': 'Q001 Single quote multiline found but double quotes preferred'},
+            {'col': 22, 'line': 5, 'message': 'Q001 Single quote multiline found but double quotes preferred'},
         ])
 
         multiline_checker = QuoteChecker(
@@ -220,9 +216,9 @@ class DocstringTestChecks(TestCase):
             filename=get_absolute_path('data/docstring_singles_function.py')
         )
         self.assertEqual(list(multiline_checker.get_quotes_errors(multiline_checker.get_file_contents())), [
-            {'col': 4, 'line': 3, 'message': message},
-            {'col': 4, 'line': 11, 'message': message},
-            {'col': 38, 'line': 15, 'message': message},
-            {'col': 4, 'line': 17, 'message': message},
-            {'col': 4, 'line': 21, 'message': message},
+            {'col': 4, 'line': 3, 'message': 'Q001 Single quote multiline found but double quotes preferred'},
+            {'col': 4, 'line': 11, 'message': 'Q001 Single quote multiline found but double quotes preferred'},
+            {'col': 38, 'line': 15, 'message': 'Q001 Single quote multiline found but double quotes preferred'},
+            {'col': 4, 'line': 17, 'message': 'Q001 Single quote multiline found but double quotes preferred'},
+            {'col': 4, 'line': 21, 'message': 'Q001 Single quote multiline found but double quotes preferred'},
         ])
